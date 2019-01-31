059446360c8d954c3591c94f1c29ad1c41bc9f2a
Rudboy972 <46441046+rudboy@users.noreply.github.com>
2019-01-31 11:02:52 +0100

    Delete teste.js

--- teste.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-const studentRoutes = require("./student");
-app.use(studentRoutes);
-
-app.listen(3000, () => console.log("Server started"));
